353e66f33278b122d3f10136442633e5c36a4871
shifa-Alam <shifa7446@gmail.com>
2022-08-17 01:18:26 +0600

    UI Update

--- MessManagement.Web/ClientApp/src/app/member-landing/member-landing.component.ts
+++ MessManagement.Web/ClientApp/src/app/member-landing/member-landing.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
 import { Member } from '../Models/member';
 import { MemberService } from '../services/member.service';
 
@@ -10,21 +12,21 @@ import { MemberService } from '../services/member.service';
   templateUrl: './member-landing.component.html',
   styleUrls: ['./member-landing.component.css']
 })
-export class MemberLandingComponent implements OnInit,AfterViewInit  {
+export class MemberLandingComponent implements OnInit, AfterViewInit {
   dataSource!: MatTableDataSource<Member>;
-  @ViewChild(MatPaginator)paginator!: MatPaginator;
-  @ViewChild(MatSort)sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   members: Member[] = [];
   displayedColumns: string[] = [];
   color: string;
 
-  constructor(private service: MemberService) {
+  constructor(private service: MemberService, public dialog: MatDialog) {
     this.color = "orange";
   }
   ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+    // this.dataSource.paginator = this.paginator;
+    // this.dataSource.sort = this.sort;
   }
 
   ngOnInit(): void {
@@ -32,15 +34,14 @@ export class MemberLandingComponent implements OnInit,AfterViewInit  {
     this.getMembers();
   }
   setColumn() {
-    this.displayedColumns = ['id', 'name', 'mobile', 'district'];
+    this.displayedColumns = ['id', 'name', 'mobile', 'district', 'action'];
   }
-
 
   getMembers() {
     this.service.getMember().subscribe(result => {
       this.members = result;
 
-      this.dataSource = new MatTableDataSource( this.members);
+      this.dataSource = new MatTableDataSource(this.members);
     },
       error => console.error(error));
   }
@@ -51,6 +52,33 @@ export class MemberLandingComponent implements OnInit,AfterViewInit  {
 
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
+    }
+  }
+  add(){
+    
+  }
+  edit(member: any) {
+    if (member != null) {
+      this.service.updateMember(member).subscribe(result => {
+        console.log(result);
+      },
+        error => console.error(error));
+    }
+  }
+
+  delete(id: number) {
+    if (id) {
+      const dialogRef = this.dialog.open(DeleteDialogComponent);
+
+      dialogRef.afterClosed().subscribe(result => {
+        console.log(`Dialog result: ${result}`);
+        if (result) {
+          this.service.deleteMember(id).subscribe(result => {
+            console.log(result);
+          },
+            error => console.error(error));
+        }
+      });
     }
   }
 }
